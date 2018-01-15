76a6e92bbde6e713e27d3785c845b1c722d988fa
遼太郎 金 <150568@jc-21.jp>
2018-01-15 12:56:22 +0900

    ssssssssss

--- Sample001/Sample001/MainPage.xaml.cs
+++ Sample001/Sample001/MainPage.xaml.cs
@@ -33,96 +33,103 @@ namespace Sample001
 
         async void ScanButtonClicked(object sender, EventArgs s)
         {
-            var scanPage = new ZXingScannerPage()
+            try
             {
-                DefaultOverlayTopText = "バーコードを読み取ります",
-                DefaultOverlayBottomText = "",
-            };
-
-            // スキャナページを表示
-            await Navigation.PushAsync(scanPage);
-
-            scanPage.OnScanResult += async (result) =>
-            {
-                // スキャン停止
-                scanPage.IsScanning = false;
-                
-                // PopAsyncで元のページに戻り、結果をダイアログで表示
-                Device.BeginInvokeOnMainThread(async () =>
+                var scanPage = new ZXingScannerPage()
                 {
-                    await Navigation.PopAsync();
-                   // await DisplayAlert("スキャン完了", result.Text, "OK");
-                });
-                string isbncode = result.Text;
-                requestUrl = url + "&isbn=" + isbncode; //URLにISBNコードを挿入
-                var layout2 = new StackLayout { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand };
-                var scroll = new ScrollView { Orientation = ScrollOrientation.Vertical };
-                layout2.Children.Add(scroll);
-                var layout = new StackLayout { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand };
-                scroll.Content = layout;
-
-                //HTTPアクセスメソッドを呼び出す
-                string APIdata = await GetApiAsync(); //jsonをstringで受け取る
-
-                //HTTPアクセス失敗処理(404エラーとか名前解決失敗とかタイムアウトとか)
-                if (APIdata is null)
-                {
-                    await DisplayAlert("接続エラー", "接続に失敗しました", "OK");
-                }
-
-                /*
-                //レスポンス(JSON)をstringに変換-------------->しなくていい
-                Stream s = GetMemoryStream(APIdata); //GetMemoryStreamメソッド呼び出し
-                StreamReader sr = new StreamReader(s);
-                string json = sr.ReadToEnd();
-                */
-                /*
-                //デシリアライズ------------------>しなくていい
-                var rakutenBooks = JsonConvert.DeserializeObject<RakutenBooks>(json.ToString());
-                */
-
-                //パースする *重要*   パースとは、文法に従って分析する、品詞を記述する、構文解析する、などの意味を持つ英単語。
-                var json = JObject.Parse(APIdata); //stringのAPIdataをJObjectにパース
-                var Items = JArray.Parse(json["Items"].ToString()); //Itemsは配列なのでJArrayにパース
-
-                //結果を出力
-                foreach (JObject jobj in Items)
-                {
-                    //↓のように取り出す
-                    JValue titleValue = (JValue)jobj["title"];
-                    string title = (string)titleValue.Value;
-
-                    JValue titleKanaValue = (JValue)jobj["titleKana"];
-                    string titleKana = (string)titleKanaValue.Value;
-
-                    JValue itemCaptionValue = (JValue)jobj["itemCaption"];
-                    string itemCaption = (string)itemCaptionValue.Value;
-
-                    JValue gazoValue = (JValue)jobj["largeImageUrl"];
-                    string gazo = (string)gazoValue.Value;
-
-                    //書き出し
-                    layout.Children.Add(new Label { Text = $"title: { title }" });
-                    layout.Children.Add(new Label { Text = $"titleKana: { titleKana }" });
-                    layout.Children.Add(new Label { Text = $"itemCaption: { itemCaption }" });
-                    layout.Children.Add(new Image { Source = gazo });
-                    String A = gazo;
-
+                    DefaultOverlayTopText = "バーコードを読み取ります",
+                    DefaultOverlayBottomText = "",
                 };
 
-                layout.Children.Add(new Label { Text = "読み取り終了", TextColor = Color.Black });
+                // スキャナページを表示
+                await Navigation.PushAsync(scanPage);
 
-                layout.Children.Add(new Label { Text = "" });//改行
+                scanPage.OnScanResult += async (result) =>
+                {
+                    // スキャン停止
+                    scanPage.IsScanning = false;
 
-                layout.Children.Add(new Label { Text = "JSON形式で書き出す", TextColor = Color.Red });
-                layout.Children.Add(new Label { Text = json.ToString() });
+                    // PopAsyncで元のページに戻り、結果をダイアログで表示
+                    Device.BeginInvokeOnMainThread(async () =>
+                        {
+                            await Navigation.PopAsync();
+                        // await DisplayAlert("スキャン完了", result.Text, "OK");
+                    });
+                    string isbncode = result.Text;
+                    requestUrl = url + "&isbn=" + isbncode; //URLにISBNコードを挿入
+                    var layout2 = new StackLayout { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand };
+                    var scroll = new ScrollView { Orientation = ScrollOrientation.Vertical };
+                    layout2.Children.Add(scroll);
+                    var layout = new StackLayout { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand };
+                    scroll.Content = layout;
+
+                    //HTTPアクセスメソッドを呼び出す
+                    string APIdata = await GetApiAsync(); //jsonをstringで受け取る
+
+                    //HTTPアクセス失敗処理(404エラーとか名前解決失敗とかタイムアウトとか)
+                    if (APIdata is null)
+                    {
+                        await DisplayAlert("接続エラー", "接続に失敗しました", "OK");
+                    }
+
+                    /*
+                    //レスポンス(JSON)をstringに変換-------------->しなくていい
+                    Stream s = GetMemoryStream(APIdata); //GetMemoryStreamメソッド呼び出し
+                    StreamReader sr = new StreamReader(s);
+                    string json = sr.ReadToEnd();
+                    */
+                    /*
+                    //デシリアライズ------------------>しなくていい
+                    var rakutenBooks = JsonConvert.DeserializeObject<RakutenBooks>(json.ToString());
+                    */
+
+                    //パースする *重要*   パースとは、文法に従って分析する、品詞を記述する、構文解析する、などの意味を持つ英単語。
+                    var json = JObject.Parse(APIdata); //stringのAPIdataをJObjectにパース
+                    var Items = JArray.Parse(json["Items"].ToString()); //Itemsは配列なのでJArrayにパース
+
+                    //結果を出力
+                    foreach (JObject jobj in Items)
+                    {
+                        //↓のように取り出す
+                        JValue titleValue = (JValue)jobj["title"];
+                        string title = (string)titleValue.Value;
+
+                        JValue titleKanaValue = (JValue)jobj["titleKana"];
+                        string titleKana = (string)titleKanaValue.Value;
+
+                        JValue itemCaptionValue = (JValue)jobj["itemCaption"];
+                        string itemCaption = (string)itemCaptionValue.Value;
+
+                        JValue gazoValue = (JValue)jobj["largeImageUrl"];
+                        string gazo = (string)gazoValue.Value;
+
+                        //書き出し
+                        layout.Children.Add(new Label { Text = $"title: { title }" });
+                        layout.Children.Add(new Label { Text = $"titleKana: { titleKana }" });
+                        layout.Children.Add(new Label { Text = $"itemCaption: { itemCaption }" });
+                        layout.Children.Add(new Image { Source = gazo });
+                        String A = gazo;
+
+                    };
+
+                    layout.Children.Add(new Label { Text = "読み取り終了", TextColor = Color.Black });
+
+                    layout.Children.Add(new Label { Text = "" });//改行
+
+                    layout.Children.Add(new Label { Text = "JSON形式で書き出す", TextColor = Color.Red });
+                    layout.Children.Add(new Label { Text = json.ToString() });
 
 
 
 
-                Content = layout2;
+                    Content = layout2;
 
-            };
+                };
+            }
+            catch (Exception e)
+            {
+                await DisplayAlert("Error", e.ToString(), "ok");
+            }
 
 
         }
